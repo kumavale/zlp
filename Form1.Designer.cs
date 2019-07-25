c1cd1b682ddc492f4b5940d80884e6ac4d3de755
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2019-07-25 22:18:11 +0900

    :tada:First import

---
+++ Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace zerodori_listening_player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_start_stop = new System.Windows.Forms.Button();
+            this.button_rewind = new System.Windows.Forms.Button();
+            this.button_forward = new System.Windows.Forms.Button();
+            this.button_next = new System.Windows.Forms.Button();
+            this.button_prev = new System.Windows.Forms.Button();
+            this.bar_seek = new System.Windows.Forms.TrackBar();
+            this.mp3_number = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_loop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bar_volume = new System.Windows.Forms.TrackBar();
+            this.time_now = new System.Windows.Forms.Label();
+            this.time_length = new System.Windows.Forms.Label();
+            this.list_speed = new System.Windows.Forms.ComboBox();
+            this.label_title = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bar_seek)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bar_volume)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_start_stop
+            // 
+            this.button_start_stop.Location = new System.Drawing.Point(113, 80);
+            this.button_start_stop.Margin = new System.Windows.Forms.Padding(0);
+            this.button_start_stop.Name = "button_start_stop";
+            this.button_start_stop.Size = new System.Drawing.Size(32, 32);
+            this.button_start_stop.TabIndex = 0;
+            this.button_start_stop.Text = "▷";
+            this.button_start_stop.UseVisualStyleBackColor = true;
+            this.button_start_stop.Click += new System.EventHandler(this.button_start_stop_Click);
+            // 
+            // button_rewind
+            // 
+            this.button_rewind.Location = new System.Drawing.Point(63, 80);
+            this.button_rewind.Name = "button_rewind";
+            this.button_rewind.Size = new System.Drawing.Size(32, 32);
+            this.button_rewind.TabIndex = 1;
+            this.button_rewind.Text = "<";
+            this.button_rewind.UseVisualStyleBackColor = true;
+            this.button_rewind.Click += new System.EventHandler(this.button_rewind_Click);
+            // 
+            // button_forward
+            // 
+            this.button_forward.Location = new System.Drawing.Point(163, 80);
+            this.button_forward.Name = "button_forward";
+            this.button_forward.Size = new System.Drawing.Size(32, 32);
+            this.button_forward.TabIndex = 2;
+            this.button_forward.Text = ">";
+            this.button_forward.UseVisualStyleBackColor = true;
+            this.button_forward.Click += new System.EventHandler(this.button_forward_Click);
+            // 
+            // button_next
+            // 
+            this.button_next.Location = new System.Drawing.Point(213, 80);
+            this.button_next.Name = "button_next";
+            this.button_next.Size = new System.Drawing.Size(32, 32);
+            this.button_next.TabIndex = 3;
+            this.button_next.Text = ">|";
+            this.button_next.UseVisualStyleBackColor = true;
+            this.button_next.Click += new System.EventHandler(this.button_next_Click);
+            // 
+            // button_prev
+            // 
+            this.button_prev.Location = new System.Drawing.Point(13, 80);
+            this.button_prev.Name = "button_prev";
+            this.button_prev.Size = new System.Drawing.Size(32, 32);
+            this.button_prev.TabIndex = 4;
+            this.button_prev.Text = "|<";
+            this.button_prev.UseVisualStyleBackColor = true;
+            this.button_prev.Click += new System.EventHandler(this.button_prev_Click);
+            // 
+            // bar_seek
+            // 
+            this.bar_seek.Location = new System.Drawing.Point(74, 124);
+            this.bar_seek.Maximum = 100;
+            this.bar_seek.Name = "bar_seek";
+            this.bar_seek.Size = new System.Drawing.Size(160, 45);
+            this.bar_seek.TabIndex = 5;
+            this.bar_seek.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.bar_seek.KeyDown += new System.Windows.Forms.KeyEventHandler(this.bar_seek_KeyDown);
+            this.bar_seek.MouseUp += new System.Windows.Forms.MouseEventHandler(this.bar_seek_MouseUp);
+            // 
+            // mp3_number
+            // 
+            this.mp3_number.Font = new System.Drawing.Font("MS Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.mp3_number.Location = new System.Drawing.Point(41, 12);
+            this.mp3_number.MaxLength = 3;
+            this.mp3_number.Name = "mp3_number";
+            this.mp3_number.Size = new System.Drawing.Size(40, 20);
+            this.mp3_number.TabIndex = 7;
+            this.mp3_number.Text = "001";
+            this.mp3_number.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.mp3_number.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.mp3_number_KeyPress);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(22, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button_loop
+            // 
+            this.button_loop.Location = new System.Drawing.Point(263, 80);
+            this.button_loop.Name = "button_loop";
+            this.button_loop.Size = new System.Drawing.Size(32, 32);
+            this.button_loop.TabIndex = 9;
+            this.button_loop.Text = "↺";
+            this.button_loop.UseVisualStyleBackColor = true;
+            this.button_loop.Click += new System.EventHandler(this.button_loop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "speed";
+            // 
+            // bar_volume
+            // 
+            this.bar_volume.Location = new System.Drawing.Point(213, 12);
+            this.bar_volume.Maximum = 100;
+            this.bar_volume.Name = "bar_volume";
+            this.bar_volume.Size = new System.Drawing.Size(67, 45);
+            this.bar_volume.TabIndex = 12;
+            this.bar_volume.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.bar_volume.Value = 50;
+            this.bar_volume.Scroll += new System.EventHandler(this.bar_volume_Scroll);
+            this.bar_volume.KeyDown += new System.Windows.Forms.KeyEventHandler(this.bar_volume_KeyDown);
+            // 
+            // time_now
+            // 
+            this.time_now.AutoSize = true;
+            this.time_now.Location = new System.Drawing.Point(38, 128);
+            this.time_now.Name = "time_now";
+            this.time_now.Size = new System.Drawing.Size(28, 13);
+            this.time_now.TabIndex = 13;
+            this.time_now.Text = "0:00";
+            // 
+            // time_length
+            // 
+            this.time_length.AutoSize = true;
+            this.time_length.Location = new System.Drawing.Point(240, 128);
+            this.time_length.Name = "time_length";
+            this.time_length.Size = new System.Drawing.Size(34, 13);
+            this.time_length.TabIndex = 14;
+            this.time_length.Text = "12:34";
+            // 
+            // list_speed
+            // 
+            this.list_speed.FormattingEnabled = true;
+            this.list_speed.Items.AddRange(new object[] {
+            "0.5",
+            "0.75",
+            "1.0",
+            "1.25",
+            "1.5",
+            "1.75",
+            "2.0"});
+            this.list_speed.Location = new System.Drawing.Point(139, 12);
+            this.list_speed.Name = "list_speed";
+            this.list_speed.Size = new System.Drawing.Size(56, 21);
+            this.list_speed.TabIndex = 15;
+            this.list_speed.SelectedIndexChanged += new System.EventHandler(this.list_speed_SelectedIndexChanged);
+            this.list_speed.KeyDown += new System.Windows.Forms.KeyEventHandler(this.list_speed_KeyDown);
+            // 
+            // label_title
+            // 
+            this.label_title.AutoSize = true;
+            this.label_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_title.Location = new System.Drawing.Point(10, 48);
+            this.label_title.Name = "label_title";
+            this.label_title.Size = new System.Drawing.Size(174, 16);
+            this.label_title.TabIndex = 16;
+            this.label_title.Text = "001_Part 2とは？_Directions";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 153);
+            this.Controls.Add(this.label_title);
+            this.Controls.Add(this.list_speed);
+            this.Controls.Add(this.time_length);
+            this.Controls.Add(this.time_now);
+            this.Controls.Add(this.bar_volume);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_loop);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.mp3_number);
+            this.Controls.Add(this.bar_seek);
+            this.Controls.Add(this.button_prev);
+            this.Controls.Add(this.button_next);
+            this.Controls.Add(this.button_forward);
+            this.Controls.Add(this.button_rewind);
+            this.Controls.Add(this.button_start_stop);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "zlp";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.bar_seek)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bar_volume)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_start_stop;
+        private System.Windows.Forms.Button button_rewind;
+        private System.Windows.Forms.Button button_forward;
+        private System.Windows.Forms.Button button_next;
+        private System.Windows.Forms.Button button_prev;
+        private System.Windows.Forms.TrackBar bar_seek;
+        private System.Windows.Forms.TextBox mp3_number;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_loop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar bar_volume;
+        private System.Windows.Forms.Label time_now;
+        private System.Windows.Forms.Label time_length;
+        private System.Windows.Forms.ComboBox list_speed;
+        private System.Windows.Forms.Label label_title;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using WMPLib;
+using Shell32;
+using System.Collections;
+using System.Text.RegularExpressions;
+using System.Configuration;
+
+namespace zerodori_listening_player
+{
+    public partial class Form1 : Form
+    {
+        const int TITLE  = 21;
+        const int LENGTH = 27;
+        enum SHIFT {
+            PREV,
+            NEXT
+        }
+
+        Timer timer_ = new Timer();
+
+        bool is_loop;
+        bool playing;
+
+        const string mp3_dir = @"sounds";
+        string   mp3_file;
+        string[] mp3_file_paths;
+        int      mp3_now;         // 現在選択されている番号
+        int      mp3_length;      // 再生時間(秒)
+        int      mp3_count;       // 音声ファイルの最大数(追加可能)
+
+
+        WindowsMediaPlayer mp = new WindowsMediaPlayer();
+
+        Shell sh = new Shell();
+        Folder f;
+        FolderItem fi;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+
+			// フォームが表示されたときの処理
+			Shown += delegate
+			{
+				//シークバーにフォーカスがあると、再生位置が表示されないのでここで再生ボタンにフォーカスを与える
+				button_start_stop.Focus();
+			};
+
+            // 自動再生をOFF
+            mp.settings.autoStart = false;
+
+			// 再生位置のシーク
+			bar_seek.ValueChanged += delegate
+			{
+				if (bar_seek.Focused)
+				{
+					//mp.controls.currentPosition = (double)bar_seek.Value / 100;
+					mp.controls.currentPosition = (double)bar_seek.Value;
+				}
+			};
+
+            timer_.Interval = 100;
+            timer_.Tick += delegate {
+                // 再生位置, 再生時間の表示
+                if(mp.playState == WMPPlayState.wmppsPlaying || mp.playState == WMPPlayState.wmppsPaused) {
+                    bar_seek.Maximum = (int)(mp.controls.currentItem.duration);
+                    bar_seek.Value   = (int)(mp.controls.currentPosition);
+                    time_now.Text = ((int)mp.controls.currentPosition / 60).ToString("0") + ":" + ((int)mp.controls.currentPosition % 60).ToString("00");
+                }
+
+                // ループ再生
+                if (is_loop && mp.playState == WMPPlayState.wmppsStopped) {
+                    mp.settings.rate = double.Parse(list_speed.Text);
+                    mp.controls.play();
+                }
+            };
+            timer_.Start();
+
+
+            // レイアウトの調整
+            // 音量バーを最背面へ
+            bar_volume.SendToBack();
+            // 速度ドロップダウンへの入力を禁止
+            list_speed.DropDownStyle = ComboBoxStyle.DropDownList;
+            // 音量バーのつまみの画像
+            //bar_volume.Thumbs
+            // フォームのサイズを固定
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            // 初期値の指定
+            list_speed.SelectedIndex = 2; // 1.0
+            is_loop = false;
+            playing = false;
+            mp3_now = 1;
+
+            // 音声ファイル一覧の読み込み
+            mp3_count = Directory.GetFiles(mp3_dir, "*.mp3", SearchOption.AllDirectories).Length;
+            if(mp3_count == 0) {
+                MessageBox.Show("音声ファイルが見つかりません",
+                    "error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            mp3_file_paths = Directory.GetFiles(mp3_dir, "*.mp3", SearchOption.AllDirectories);
+            Array.Sort(mp3_file_paths, new SortByNumber());
+
+            // 前回の設定を読み込み, 適用
+            mp3_now = int.Parse(ConfigurationManager.AppSettings["now"]);
+            mp3_file = mp3_now.ToString("000") + ".mp3";
+            list_speed.SelectedIndex = int.Parse(ConfigurationManager.AppSettings["speed"]);
+            mp.settings.volume = int.Parse(ConfigurationManager.AppSettings["volume"]);
+            bar_volume.Value = mp.settings.volume;
+            is_loop = bool.Parse(ConfigurationManager.AppSettings["loop"]);
+            button_loop.BackColor = is_loop ? Color.Gray : SystemColors.Control;
+            mp.URL = mp3_file_paths[mp3_now - 1];
+
+            init();
+        }
+
+        private void button_start_stop_Click(object sender, EventArgs e)
+        {
+            playing = !playing;
+            mp_ctl();
+        }
+
+        private void button_loop_Click(object sender, EventArgs e)
+        {
+            is_loop = !is_loop;
+            button_loop.BackColor = is_loop ? Color.Gray : SystemColors.Control;
+        }
+
+        private void change_mp3()
+        {
+            mp3_file = mp3_now.ToString("000") + ".mp3";
+            //mp.URL = mp3_dir + "\\" + mp3_file;
+            mp.URL = mp3_file_paths[mp3_now - 1];
+            mp.settings.rate = double.Parse(list_speed.Text);
+            f = sh.NameSpace(Path.GetDirectoryName(mp.URL));
+            fi = f.ParseName(mp3_file);
+            mp3_number.Text = mp3_now.ToString("000");
+            playing = false;
+        }
+
+        private void button_next_Click(object sender, EventArgs e)
+        {
+            shift_sound(SHIFT.NEXT);
+        }
+
+        private void button_prev_Click(object sender, EventArgs e)
+        {
+            shift_sound(SHIFT.PREV);
+        }
+        private void shift_sound(SHIFT s)
+        {
+            if (s == SHIFT.PREV) {
+                --mp3_now;
+                if (mp3_now == 0)
+                    mp3_now = mp3_count;
+            }
+            else if (s == SHIFT.NEXT) {
+                mp3_now = mp3_now % mp3_count + 1;
+            }
+
+            bar_seek.Value = 0;
+            time_now.Text = "0:00";
+            playing = false;
+            mp_ctl();
+            change_mp3();
+            set_title();
+            set_time();
+        }
+
+        private void set_title()
+        {
+            label_title.Text = f.GetDetailsOf(fi, TITLE);
+        }
+
+        private void set_time()
+        {
+            DateTime dt = DateTime.Parse(f.GetDetailsOf(fi, LENGTH));
+            time_length.Text = dt.Minute + ":" + dt.Second;
+            mp3_length = dt.Minute * 60 + dt.Second;
+        }
+
+        private void mp_ctl()
+        {
+            if(playing) {
+                mp.controls.play();
+                button_start_stop.Text = "| |";  // 仮 TODO 画像
+            } else {
+                mp.controls.pause();
+                button_start_stop.Text = "▷";  // 仮 TODO 画像
+            }
+        }
+
+        private void init()
+        {
+            time_now.Text = "0:00";
+            change_mp3();
+            set_title();
+            set_time();
+            mp.controls.pause();
+        }
+
+        private void mp3_number_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char key = e.KeyChar;
+
+            if((e.KeyChar < '0' || '9' < e.KeyChar) && e.KeyChar != '\b') {
+                e.Handled = true;
+            }
+
+            //if('０' <= key && key <= '９')
+            //{
+            //    key = (char)(key - '０' + '0');
+            //    mp3_number.Text += key;
+            //    this.mp3_number.Select(this.mp3_number.Text.Length, 0);
+            //}
+
+            if(e.KeyChar == (char)Keys.Enter) {
+                button_start_stop.Focus();
+                mp3_number.Text = mp3_number.Text.PadLeft(3, '0');
+                mp3_now = int.Parse(mp3_number.Text);
+
+                if(mp3_now < 1 || mp3_count < mp3_now) {
+                    mp3_number.Text = "001";
+                    mp3_now = 1;
+                }
+
+                mp3_number.Select(mp3_number.Text.Length, 0);
+
+                change_mp3();
+                set_time();
+                set_title();
+            }
+        }
+
+        private void bar_volume_Scroll(object sender, EventArgs e)
+        {
+            mp.settings.volume = bar_volume.Value;
+        }
+
+        [System.Security.Permissions.UIPermission(
+            System.Security.Permissions.SecurityAction.Demand,
+            Window = System.Security.Permissions.UIPermissionWindow.AllWindows)]
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+            // 左キーで 5秒戻り
+            if ((keyData & Keys.KeyCode) == Keys.Left) {
+                mp.controls.currentPosition -= 5.0;
+                return true;
+            }
+            // 右キーで 5秒送り
+            else if ((keyData & Keys.KeyCode) == Keys.Right) {
+                mp.controls.currentPosition += 5.0;
+                return true;
+            }
+            // Pキーで 前の音声
+            else if ((keyData & Keys.KeyCode) == Keys.P) {
+                shift_sound(SHIFT.PREV);
+                return true;
+            }
+            // Nキーで 次の音声
+            else if ((keyData & Keys.KeyCode) == Keys.N) {
+                shift_sound(SHIFT.NEXT);
+                return true;
+            }
+            // Lキーで ループ on/off
+            else if ((keyData & Keys.KeyCode) == Keys.L) {
+                is_loop = !is_loop;
+                button_loop.BackColor = is_loop ? Color.Gray : SystemColors.Control;
+                return true;
+            }
+            // スペースキーでstart/stop
+            else if ((keyData & Keys.KeyCode) == Keys.Space) {
+                playing = !playing;
+                mp_ctl();
+                return true;
+            }
+
+            return base.ProcessDialogKey(keyData);
+        }
+
+        private void list_speed_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down || e.KeyCode == Keys.Up || e.KeyCode == Keys.Right || e.KeyCode == Keys.Left)
+                e.Handled = true;
+        }
+
+        private void bar_volume_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down || e.KeyCode == Keys.Up || e.KeyCode == Keys.Right || e.KeyCode == Keys.Left)
+                e.Handled = true;
+        }
+
+        private void bar_seek_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down || e.KeyCode == Keys.Up || e.KeyCode == Keys.Right || e.KeyCode == Keys.Left)
+                e.Handled = true;
+        }
+
+        private void bar_seek_MouseUp(object sender, MouseEventArgs e)
+        {
+            button_start_stop.Focus();
+        }
+
+        private void list_speed_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            mp.settings.rate = double.Parse(list_speed.Text);
+        }
+
+        private void button_rewind_Click(object sender, EventArgs e)
+        {
+            mp.controls.currentPosition -= 5.0;
+        }
+
+        private void button_forward_Click(object sender, EventArgs e)
+        {
+            mp.controls.currentPosition += 5.0;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Configuration cfg = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            cfg.AppSettings.Settings["now"].Value = mp3_now.ToString();
+            cfg.AppSettings.Settings["speed"].Value = list_speed.SelectedIndex.ToString();
+            cfg.AppSettings.Settings["volume"].Value = mp.settings.volume.ToString();
+            cfg.AppSettings.Settings["loop"].Value = is_loop.ToString();
+
+            cfg.Save();
+        }
+    }
+
+    public class SortByNumber : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            string s1 = (string)x;
+            string s2 = (string)y;
+            int num1, num2;
+
+            Regex re = new Regex(@"[^0-9]");
+
+            s1 = re.Replace(Path.GetFileName(s1), "");
+            s2 = re.Replace(Path.GetFileName(s2), "");
+
+            num1 = int.Parse(s1);
+            num2 = int.Parse(s2);
+
+            return num1 - num2;
+        }
+    }
+}
